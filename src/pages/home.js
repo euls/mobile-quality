b918c8692d6c2e79cae26e54236d80ecc86ee3c1
elus <lucky123>
2018-07-11 17:26:31 +0800

    重构

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,90 @@
 import React, {Component} from 'react';
-import {Card, Modal, Button, Input, Form} from 'antd';
-import {Link } from 'react-router-dom'
+import {Row, Col, Upload, Input, Form, Icon} from 'antd';
+import {Link} from 'react-router-dom'
+
 const FormItem = Form.Item;
 export default class home extends Component {
     state = {
+        thumbUrl: null,
+        images: [],
         result: null,
         assessment: null,
-        screen:null,
-        frame:null,
-        back:null,
-        angles:null,
+        screen: null,
+        frame: null,
+        back: null,
+        user: null,
+        time: null,
+        model: null,
+        color: null,
+        type: null,
+        history: null,
+        system: null,
+        vision: null,
+        size: null,
+        imei: null,
+        year: null,
+        other: null
     }
-
+    onSystemChange = (evt) => {
+        this.setState({
+            system: evt.target.value,
+        });
+    }
+    onUserChange = (evt) => {
+        this.setState({
+            user: evt.target.value,
+        });
+    }
+    onTimeChange = (evt) => {
+        this.setState({
+            time: evt.target.value,
+        });
+    }
+    onModelChange = (evt) => {
+        this.setState({
+            model: evt.target.value,
+        });
+    }
+    onColorChange = (evt) => {
+        this.setState({
+            color: evt.target.value,
+        });
+    }
+    onTypeChange = (evt) => {
+        this.setState({
+            type: evt.target.value,
+        });
+    }
+    onHistoryChange = (evt) => {
+        this.setState({
+            history: evt.target.value,
+        });
+    }
+    onVisionChange = (evt) => {
+        this.setState({
+            vision: evt.target.value,
+        });
+    }
+    onSizeChange = (evt) => {
+        this.setState({
+            size: evt.target.value,
+        });
+    }
+    onImeiChange = (evt) => {
+        this.setState({
+            imei: evt.target.value,
+        });
+    }
+    onYearChange = (evt) => {
+        this.setState({
+            year: evt.target.value,
+        });
+    }
+    onOtherChange = (evt) => {
+        this.setState({
+            other: evt.target.value,
+        });
+    }
     onResultChange = (evt) => {
         this.setState({
             result: evt.target.value,
@@ -42,35 +115,152 @@ export default class home extends Component {
             angles: evt.target.value,
         });
     }
+    handleChange = ({fileList}) => {
+        this.setState({images: fileList})
+        if(fileList.length>0){
+            var url = window.URL || window.webkitURL || window.mozURL;
+            var src = url.createObjectURL(fileList[0].originFileObj);
+            this.setState({thumbUrl: src});
+        }
+    }
 
     render() {
         const {visible, loading} = this.state;
+
+        const uploadButton = (
+            <div>
+                <Icon type="plus"/>
+                <div className="ant-upload-text">Upload</div>
+            </div>
+        );
         return (
             <div>
-                <Form style={{width:'20rem',marginLeft: 'auto', marginRight: 'auto'}}>
-                    <FormItem>
-                        <Input value={this.state.result}  onChange={this.onResultChange} placeholder="检测结果"/>
-                    </FormItem>
-                    <FormItem>
-                        <Input value={this.state.assessment}  onChange={this.onAssessmentChange} placeholder="整机评估"/>
-                    </FormItem>
-                    <FormItem>
-                        <Input value={this.state.screen}  onChange={this.onScreenChange} placeholder="屏幕检测"/>
-                    </FormItem>
-                    <FormItem>
-                        <Input value={this.state.frame}  onChange={this.onFrameChange} placeholder="边框检测"/>
-                    </FormItem>
-                    <FormItem>
-                        <Input value={this.state.back}  onChange={this.onBackChange} placeholder="背部检测"/>
-                    </FormItem>
-                    <FormItem>
-                        <Input value={this.state.angles}  onChange={this.onAnglesChange} placeholder="四角检测"/>
-                    </FormItem>
+                <Form style={{width: '50rem', marginLeft: 'auto', marginRight: 'auto'}}>
+                    <Row>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.user} onChange={this.onUserChange} placeholder="质检工程师"/>
+                            </FormItem>
+                        </Col>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.time} onChange={this.onTimeChange} placeholder="质检时间"/>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.model} onChange={this.onModelChange} placeholder="产品型号"/>
+                            </FormItem>
+                        </Col>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.color} onChange={this.onColorChange} placeholder="颜色"/>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.type} onChange={this.onTypeChange} placeholder="网络制式"/>
+                            </FormItem>
+                        </Col>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.history} onChange={this.onHistoryChange} placeholder="维修历史"/>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.vision} onChange={this.onVisionChange} placeholder="版本"/>
+                            </FormItem>
+                        </Col>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.size} onChange={this.onSizeChange} placeholder="容量"/>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.imei} onChange={this.onImeiChange} placeholder="IMEI"/>
+                            </FormItem>
+                        </Col>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.system} onChange={this.onSystemChange} placeholder="系统版本"/>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.year} onChange={this.onYearChange} placeholder="电池寿命"/>
+                            </FormItem>
+                        </Col>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.result} onChange={this.onResultChange} placeholder="检测结果"/>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.assessment} onChange={this.onAssessmentChange}
+                                       placeholder="整机评估"/>
+                            </FormItem>
+                        </Col>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.screen} onChange={this.onScreenChange} placeholder="屏幕检测"/>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.frame} onChange={this.onFrameChange} placeholder="边框检测"/>
+                            </FormItem>
+                        </Col>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.back} onChange={this.onBackChange} placeholder="背部检测"/>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.angles} onChange={this.onAnglesChange} placeholder="四角检测"/>
+                            </FormItem>
+                        </Col>
+                        <Col span={12}>
+                            <FormItem>
+                                <Input value={this.state.other} onChange={this.onOtherChange} placeholder="其他"/>
+                            </FormItem>
+                        </Col>
+                    </Row>
+                    <Upload
+                        listType="picture-card"
+                        fileList={this.state.images}
+                        onChange={this.handleChange}
+                    >
+                        {this.state.images.length >= 1 ? null : uploadButton}
+                    </Upload>
                     <FormItem>
                         <Link to={{
                             pathname: '/report',
                             hash: '#the-hash',
-                            state: this.state}
+                            state: {
+                                ...this.state,
+                                images:[]
+                            }
+                        }
                         }>提交</Link>
 
                     </FormItem>
--- src/pages/report.js
+++ src/pages/report.js
@@ -4,13 +4,13 @@ import {
     Card,
     Row,
     Col,
-    Tooltip,
+    Avatar,
     Divider,
 } from 'antd';
 import 'ant-design-pro/dist/ant-design-pro.css';
 import '../less/AdvancedProfile.less';
 import DescriptionList from 'ant-design-pro/lib/DescriptionList';
-import zhangUrl from '../images/IMG_zhang.png';
+import zhangUrl from '../images/IMG_3169.png';
 
 const {Description} = DescriptionList;
 
@@ -18,74 +18,108 @@ class Report extends Component {
     componentDidMount() {
         console.log(this.props.location.state)
     }
+
     render() {
         return (<div className={'App'}>
-            <Card style={{marginBottom: 24, marginTop: '2rem', width: '42rem', marginLeft: 'auto', marginRight: 'auto'}}
-                  bordered={true}>
                 <Row>
-                    <Col span={22} style={{marginLeft: 'auto', marginRight: 'auto', textAlign: 'center'}}>
-                        <h4 style={{marginBottom: 5, fontSize: '2rem'}}>检测报告</h4>
-                        <Divider style={{margin: '5px 0', width: '10rem', marginLeft: 'auto', marginRight: 'auto'}}/>
-                        <h4 style={{marginBottom: 40, fontSize: '0.8rem'}}>手机</h4>
+                    <Col span={12} style={{textAlign: 'right'}}>
+                        <Avatar shape="square" size={'large'} src={this.props.location.state.thumbUrl?this.props.location.state.thumbUrl:""}/>
                     </Col>
-                    <Col span={2}>
-                        <img alt="example" style={{width: '10rem', float: "right", zIndex: 999}} src={zhangUrl}/>
+                    <Col span={12} style={{textAlign: 'left',fontSize:'0.6rem'}}>
+                        <Row style={{marginTop: '0.5rem',paddingLeft:'1rem'}}>
+                            <span>质检工程师:{this.props.location.state.user}</span>
+                        </Row>
+                        <Row style={{paddingLeft:'1rem'}}>
+                            <span>质检时间:{this.props.location.state.time}</span>
+                        </Row>
                     </Col>
                 </Row>
-                <DescriptionList style={{marginBottom: 24}} col={1}>
-                    <Description term="检测结果">{this.props.location.state.result}</Description>
-                    <Description term="整机评估">
-                        {this.props.location.state.assessment}
-                    </Description>
-                </DescriptionList>
-
-                <Card type="inner" bordered={true}>
-                    <DescriptionList size="large" col={2}
-                                     style={{marginBottom: 24, width: '26rem', marginLeft: 'auto', marginRight: 'auto'}}
-                                     title="外观检测">
-                        <Description term="屏幕检测"> {this.props.location.state.screen}</Description>
-                        <Description term="边框检测"> {this.props.location.state.frame}</Description>
-                        <Description term="背部检测"> {this.props.location.state.back}</Description>
-                        <Description term="四角检测"> {this.props.location.state.angles}</Description>
+                <Card style={{marginTop: '2rem', width: '50rem', marginLeft: 'auto', marginRight: 'auto'}}
+                      bordered={true}>
+                    <Row>
+                        <Col span={22} style={{marginLeft: 'auto', marginRight: 'auto', textAlign: 'center'}}>
+                            <h4 style={{marginBottom: 5, fontSize: '2rem'}}>检测报告</h4>
+                        </Col>
+                        <Col span={2}>
+                            <img alt="example" style={{width: '10rem', float: "right", zIndex: 999}} src={zhangUrl}/>
+                        </Col>
+                    </Row>
+                    <DescriptionList style={{marginBottom: 24, textAlign: 'left'}} col={1}>
+                        <Description term="产品型号"> {this.props.location.state.model}</Description>
+                        <Description term="检测结果">{this.props.location.state.result}</Description>
+                        <Description term="整机评估">
+                            {this.props.location.state.assessment}
+                        </Description>
+                    </DescriptionList>
+                    <Card type="inner" bordered={true} style={{marginBottom: 20}}>
+                        <DescriptionList size="large" col={2}
+                                         style={{width: '30rem', marginLeft: 'auto', marginRight: 'auto'}}
+                                         title="基本信息">
+                            <Description term="颜色"> {this.props.location.state.color}</Description>
+                            <Description term="网络制式"> {this.props.location.state.type}</Description>
+                            <Description term="维修历史"> {this.props.location.state.history}</Description>
+                            <Description term="版本"> {this.props.location.state.vision}</Description>
+                            <Description term="容量"> {this.props.location.state.size}</Description>
+                            <Description term="IMEI"> {this.props.location.state.imei}</Description>
+                            <Description term="系统版本"> {this.props.location.state.system}</Description>
+                            <Description term="电池寿命"> {this.props.location.state.year}</Description>
+                        </DescriptionList>
+                    </Card>
+                    <Card type="inner" bordered={true}>
+                        <DescriptionList size="large" col={2}
+                                         style={{width: '26rem', marginLeft: 'auto', marginRight: 'auto'}}
+                                         title="外观检测">
+                            <Description term="屏幕检测"> {this.props.location.state.screen}</Description>
+                            <Description term="边框检测"> {this.props.location.state.frame}</Description>
+                            <Description term="背部检测"> {this.props.location.state.back}</Description>
+                            <Description term="四角检测"> {this.props.location.state.angles}</Description>
+                        </DescriptionList>
+                    </Card>
+                    <Card type="inner" bordered={false}>
+                        <DescriptionList size="large" style={{marginBottom: 32}} title="功能检测">
+                            <Description term="USB连接"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                            type="check-circle"/></Description>
+                            <Description term="触控功能"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                           type="check-circle"/></Description>
+                            <Description term="摄像头功能"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                            type="check-circle"/></Description>
+                            <Description term="耳机功能"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                           type="check-circle"/></Description>
+                            <Description term="录音功能"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                           type="check-circle"/></Description>
+                            <Description term="充放电测试"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                            type="check-circle"/></Description>
+                            <Description term="WIFI功能"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                             type="check-circle"/></Description>
+                            <Description term="蓝牙功能"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                           type="check-circle"/></Description>
+                            <Description term="重力感应"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                           type="check-circle"/></Description>
+                            <Description term="GPS导航"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                            type="check-circle"/></Description>
+                            <Description term="指纹识别"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                           type="check-circle"/></Description>
+                            <Description term="屏幕显示"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                           type="check-circle"/></Description>
+                            <Description term="外放功能"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                           type="check-circle"/></Description>
+                            <Description term="按键功能"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                           type="check-circle"/></Description>
+                        </DescriptionList>
+                    </Card>
+                    <DescriptionList
+                        style={{marginBottom: 24, width: '30rem', marginLeft: 'auto', marginRight: 'auto'}}>
+                        <Description term="无此功能"><Icon style={{marginLeft: 4}}
+                                                       type="minus-circle"/></Description>
+                        <Description term="功能异常"><Icon style={{color: 'red', marginLeft: 4}}
+                                                       type="close-circle"/></Description>
+                        <Description term="功能正常"><Icon style={{color: '#00e673', marginLeft: 4}}
+                                                       type="check-circle"/></Description>
+                    </DescriptionList>
+                    <DescriptionList style={{textAlign: 'left'}} col={1}>
+                        <Description term="其他">{this.props.location.state.other}</Description>
                     </DescriptionList>
                 </Card>
-                <Card type="inner" bordered={false}>
-                    <DescriptionList size="large" style={{marginBottom: 32}} title="功能检测">
-                        <Description term="USB连接"><Icon style={{color: '#00e673', marginLeft: 4}} type="check-circle"/></Description>
-                        <Description term="触控功能"><Icon style={{color: '#00e673', marginLeft: 4}}
-                                                       type="check-circle"/></Description>
-                        <Description term="摄像头功能"><Icon style={{color: '#00e673', marginLeft: 4}} type="check-circle"/></Description>
-                        <Description term="耳机功能"><Icon style={{color: '#00e673', marginLeft: 4}}
-                                                       type="check-circle"/></Description>
-                        <Description term="录音功能"><Icon style={{color: '#00e673', marginLeft: 4}}
-                                                       type="check-circle"/></Description>
-                        <Description term="充放电测试"><Icon style={{color: '#00e673', marginLeft: 4}} type="check-circle"/></Description>
-                        <Description term="WIFI功能"><Icon style={{color: '#00e673', marginLeft: 4}}
-                                                         type="check-circle"/></Description>
-                        <Description term="蓝牙功能"><Icon style={{color: '#00e673', marginLeft: 4}}
-                                                       type="check-circle"/></Description>
-                        <Description term="重力感应"><Icon style={{color: '#00e673', marginLeft: 4}}
-                                                       type="check-circle"/></Description>
-                        <Description term="GPS导航"><Icon style={{color: '#00e673', marginLeft: 4}} type="check-circle"/></Description>
-                        <Description term="指纹识别"><Icon style={{color: '#00e673', marginLeft: 4}}
-                                                       type="check-circle"/></Description>
-                        <Description term="屏幕显示"><Icon style={{color: '#00e673', marginLeft: 4}}
-                                                       type="check-circle"/></Description>
-                        <Description term="外放功能"><Icon style={{color: '#00e673', marginLeft: 4}}
-                                                       type="check-circle"/></Description>
-                        <Description term="按键功能"><Icon style={{color: '#00e673', marginLeft: 4}}
-                                                       type="check-circle"/></Description>
-                    </DescriptionList>
-                </Card>
-                <DescriptionList style={{marginBottom: 24, width: '30rem', marginLeft: 'auto', marginRight: 'auto'}}>
-                    <Description term="无此功能"><Icon style={{marginLeft: 4}}
-                                                   type="minus-circle"/></Description>
-                    <Description term="功能异常"><Icon style={{color: 'red', marginLeft: 4}}
-                                                   type="close-circle"/></Description>
-                    <Description term="功能正常"><Icon style={{color: '#00e673', marginLeft: 4}}
-                                                   type="check-circle"/></Description>
-                </DescriptionList>
-            </Card>
             </div>
         );
     }
