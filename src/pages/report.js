5f856e8303c23dc651fd0991fa53007e69d3200c
elus <lucky123>
2018-07-12 17:25:01 +0800

    修改格式

--- src/pages/report.js
+++ src/pages/report.js
@@ -13,7 +13,7 @@ import DescriptionList from 'ant-design-pro/lib/DescriptionList';
 import zhangUrl from '../images/IMG_3169.png';
 
 const {Description} = DescriptionList;
-
+const { Meta } = Card;
 class Report extends Component {
     componentDidMount() {
         console.log(this.props.location.state)
@@ -21,15 +21,16 @@ class Report extends Component {
 
     render() {
         return (<div className={'App'}>
-                <Row>
+                <Row style={{paddingTop:'2rem'}}>
                     <Col span={12} style={{textAlign: 'right'}}>
-                        <Avatar shape="square" size={'large'} src={this.props.location.state.thumbUrl?this.props.location.state.thumbUrl:""}/>
+                        <Avatar style={{width: '4rem', height: '4rem',float: "right"}} shape="square" size={'large'}
+                                src={this.props.location.state.thumbUrl ? this.props.location.state.thumbUrl : ""}/>
                     </Col>
-                    <Col span={12} style={{textAlign: 'left',fontSize:'0.6rem'}}>
-                        <Row style={{marginTop: '0.5rem',paddingLeft:'1rem'}}>
-                            <span>质检工程师:{this.props.location.state.user}</span>
+                    <Col span={12} style={{textAlign: 'left', fontSize: '1rem'}}>
+                        <Row style={{marginTop: '0.5rem', paddingLeft: '1rem'}}>
+                            <span style={{backgroundColor:'red',color:'white'}}>质检工程师:{this.props.location.state.user}</span>
                         </Row>
-                        <Row style={{paddingLeft:'1rem'}}>
+                        <Row style={{marginTop: '0.5rem',paddingLeft: '1rem'}}>
                             <span>质检时间:{this.props.location.state.time}</span>
                         </Row>
                     </Col>
@@ -38,13 +39,13 @@ class Report extends Component {
                       bordered={true}>
                     <Row>
                         <Col span={22} style={{marginLeft: 'auto', marginRight: 'auto', textAlign: 'center'}}>
-                            <h4 style={{marginBottom: 5, fontSize: '2rem'}}>检测报告</h4>
+                            <h4 style={{marginBottom: 5, fontSize: '1.6em'}}>检测报告</h4>
                         </Col>
                         <Col span={2}>
-                            <img alt="example" style={{width: '10rem', float: "right", zIndex: 999}} src={zhangUrl}/>
+                            <img alt="example" style={{width: '8rem', float: "right", zIndex: 999}} src={zhangUrl}/>
                         </Col>
                     </Row>
-                    <DescriptionList style={{marginBottom: 24, textAlign: 'left'}} col={1}>
+                    <DescriptionList style={{marginBottom: 12, textAlign: 'left'}} col={1}>
                         <Description term="产品型号"> {this.props.location.state.model}</Description>
                         <Description term="检测结果">{this.props.location.state.result}</Description>
                         <Description term="整机评估">
@@ -57,10 +58,22 @@ class Report extends Component {
                                          title="基本信息">
                             <Description term="颜色"> {this.props.location.state.color}</Description>
                             <Description term="网络制式"> {this.props.location.state.type}</Description>
+                        </DescriptionList>
+                        <Divider style={{ margin: '0.2rem 0.2rem' }} />
+                        <DescriptionList size="large" col={2}
+                                         style={{width: '30rem', marginLeft: 'auto', marginRight: 'auto'}}>
                             <Description term="维修历史"> {this.props.location.state.history}</Description>
                             <Description term="版本"> {this.props.location.state.vision}</Description>
+                        </DescriptionList>
+                        <Divider style={{ margin: '0.2rem 0.2rem' }} />
+                        <DescriptionList size="large" col={2}
+                                         style={{width: '30rem', marginLeft: 'auto', marginRight: 'auto'}}>
                             <Description term="容量"> {this.props.location.state.size}</Description>
                             <Description term="IMEI"> {this.props.location.state.imei}</Description>
+                        </DescriptionList>
+                        <Divider style={{ margin: '0.2rem 0.2rem' }} />
+                        <DescriptionList size="large" col={2}
+                                         style={{width: '30rem', marginLeft: 'auto', marginRight: 'auto'}}>
                             <Description term="系统版本"> {this.props.location.state.system}</Description>
                             <Description term="电池寿命"> {this.props.location.state.year}</Description>
                         </DescriptionList>
@@ -76,7 +89,7 @@ class Report extends Component {
                         </DescriptionList>
                     </Card>
                     <Card type="inner" bordered={false}>
-                        <DescriptionList size="large" style={{marginBottom: 32}} title="功能检测">
+                        <DescriptionList size="large" style={{marginBottom: 24}} title="功能检测">
                             <Description term="USB连接"><Icon style={{color: '#00e673', marginLeft: 4}}
                                                             type="check-circle"/></Description>
                             <Description term="触控功能"><Icon style={{color: '#00e673', marginLeft: 4}}
@@ -108,7 +121,7 @@ class Report extends Component {
                         </DescriptionList>
                     </Card>
                     <DescriptionList
-                        style={{marginBottom: 24, width: '30rem', marginLeft: 'auto', marginRight: 'auto'}}>
+                        style={{marginBottom: 12, width: '30rem', marginLeft: 'auto', marginRight: 'auto'}}>
                         <Description term="无此功能"><Icon style={{marginLeft: 4}}
                                                        type="minus-circle"/></Description>
                         <Description term="功能异常"><Icon style={{color: 'red', marginLeft: 4}}
